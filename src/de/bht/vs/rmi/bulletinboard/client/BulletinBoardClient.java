15cd12693d3dec17a41bb95007e4f084afa33b9f
Timo Raschke <timo.raschke@gmail.com>
2015-11-09 12:58:45 +0100

    Added packages and dummy classes

---
+++ src/de/bht/vs/rmi/bulletinboard/client/BulletinBoardClient.java
@@ -0,0 +1,7 @@
+package de.bht.vs.rmi.bulletinboard.client;
+
+/**
+ * Created by Timo on 09.11.2015.
+ */
+public class BulletinBoardClient {
+}
---
+++ src/de/bht/vs/rmi/bulletinboard/server/BulletinBoardServer.java
@@ -0,0 +1,7 @@
+package de.bht.vs.rmi.bulletinboard.server;
+
+/**
+ * Created by Timo on 09.11.2015.
+ */
+public class BulletinBoardServer {
+}
