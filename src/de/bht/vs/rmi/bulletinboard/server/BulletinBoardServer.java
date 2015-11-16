3be3c3f62d90f39409c69bf12ee3b8bd91ae5b23
Timo Raschke <timo.raschke@gmail.com>
2015-11-16 13:22:59 +0100

    BulletinBoardServer: Logs and JavaDocs

--- src/de/bht/vs/rmi/bulletinboard/server/BulletinBoardServer.java
+++ src/de/bht/vs/rmi/bulletinboard/server/BulletinBoardServer.java
@@ -6,12 +6,14 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by Timo on 09.11.2015.
  */
 public class BulletinBoardServer extends UnicastRemoteObject implements BulletinBoardServerInterface {
 
+    private final static Logger LOGGER = Logger.getLogger(BulletinBoardServer.class.getName());
     public static final int maxNumMessages = 20;
     public static final int messageLifeTime = 10 * 60;
     public static final int maxLengthMessage = 160;
@@ -29,41 +31,51 @@ public class BulletinBoardServer extends UnicastRemoteObject implements Bulletin
     @Override
     public int login(String password) throws RemoteException {
         if (password.equals(this.psk)) {
+            LOGGER.info("Password correct. Client logged in.");
             loggedIn = true;
             return 1;
         }
+        LOGGER.info("Password incorrect.");
         return 0;
     }
 
     @Override
     public int getMessageCount() throws RemoteException {
         if (loggedIn) {
+            LOGGER.info("Messages count: " + this.messages.size());
             return this.messages.size();
         }
+        LOGGER.warning("Client not logged in. Access denied.");
         return -1;
     }
 
     @Override
     public String[] getMessages() throws RemoteException {
         if (loggedIn) {
+            LOGGER.info("Messages: " + this.messages.toArray(new String[messages.size()]));
             return this.messages.toArray(new String[messages.size()]);
         }
-        return new String[0];
+        LOGGER.warning("Client not logged in. Access denied.");
+        return null;
     }
 
     @Override
     public String getMessage(int index) throws RemoteException {
         if (loggedIn) {
+            LOGGER.info("Message: " + this.messages.get(index));
             return this.messages.get(index);
         }
+        LOGGER.warning("Client not logged in. Access denied.");
         return null;
     }
 
     @Override
     public boolean putMessage(String msg) throws RemoteException {
         if (loggedIn) {
+            LOGGER.info("Message \"" + msg + "\" added to bulletin board.");
             this.messages.add(msg);
         }
+        LOGGER.warning("Client not logged in. Access denied.");
         return false;
     }
 
@@ -71,6 +83,6 @@ public class BulletinBoardServer extends UnicastRemoteObject implements Bulletin
         BulletinBoardServer bulletinBoardServer = new BulletinBoardServer();
         String url = "rmi://localhost/" + BulletinBoardServer.nameOfService;
         Naming.rebind(url, bulletinBoardServer);
-        System.out.println("BulletinServer bound. Access at: " + url);
+        LOGGER.info("BulletinServer bound. Access at: " + url);
     }
 }
--- src/de/bht/vs/rmi/bulletinboard/server/BulletinBoardServerInterface.java
+++ src/de/bht/vs/rmi/bulletinboard/server/BulletinBoardServerInterface.java
@@ -1,14 +1,47 @@
 package de.bht.vs.rmi.bulletinboard.server;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
  * Created by Timo on 09.11.2015.
  */
-public interface BulletinBoardServerInterface {
+public interface BulletinBoardServerInterface extends Remote {
+    /**
+     * Logs in a client.
+     * @param password The server password.
+     * @return 1 for success, 0 for failure.
+     * @throws RemoteException
+     */
     int login (String password) throws RemoteException;
+
+    /**
+     * Gets the the current count of messages.
+     * @return The current count of messages. -1 if user is not logged in.
+     * @throws RemoteException
+     */
     int getMessageCount() throws RemoteException;
+
+    /**
+     * Gets all messages from server.
+     * @return Array with all messages as strings. Null if user is not logged in.
+     * @throws RemoteException
+     */
     String[] getMessages() throws RemoteException;
+
+    /**
+     * Gets message at position.
+     * @param index The position.
+     * @return Message at index as string. Null if user is not logged in.
+     * @throws RemoteException
+     */
     String getMessage(int index) throws RemoteException;
+
+    /**
+     * Put message to the bulletin board.
+     * @param msg The Messages as string.
+     * @return true if message is posted, false if an error appeared.
+     * @throws RemoteException
+     */
     boolean putMessage(String msg) throws RemoteException;
 }
